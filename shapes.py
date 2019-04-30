15f5572b791cf718ed4a31454c2b01beddb3b493
Mercykylax <nyawiramercy49@gmail.com>
2019-04-30 19:26:59 +0300

    Test 1

---
+++ shapes.py
@@ -0,0 +1,58 @@
+class Circle:
+	def__init__(self,area,radius,circumfrence):
+	    self.radius=radius
+	    self.area=area
+	    self.circumfrence=circumfrence
+
+	def area(self,area):
+		self.area = 3.142*r^2
+        print(area)
+
+    def circumfrence(self,circumfrence):
+        self.circumfrence=3.14*r+r
+        print(circumfrence)
+
+class Square:
+    def__init__(self,square,area,perimeter):
+        self.square=square
+        self.area=area
+        self.perimeter=perimeter
+
+    def area=(self.area):
+    	self.area=3.14*r*r^2
+    	print(area)
+
+    def perimeter=(self,perimeter):
+        self.perimeter=3.14*r+r
+        print(perimeter)
+
+class Rectangle:
+    def__init__(self,sides,area,perimeter):
+        self.sides=sides
+        self.area=area
+        self.perimeter=perimeter
+
+    def area=(self,area):
+    	self.area=l*w
+    	print(area)
+
+    def perimeter=(self,perimeter):
+    	self.perimeter=2(l+w)
+    	print(perimeter)
+
+class Sphere:
+	def__init__(self,radius,surface_area,volume)
+	    self.radius=radius
+	    self.surface_area=surface_area
+	    self.volume=volume
+	    
+	def surface_area=(self,surface_area) :
+	    self.surface_area=4*3.14*r^2
+	    print(surface_area) 
+
+    def volume=(self,volume):
+    	self.volume=4/3(3.14*r^3)
+    	print(volume)
+        	
+
+
